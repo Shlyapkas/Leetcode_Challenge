20e0fe100b40bbe4a68019f22b5beaadfefd5253
Unitmaps <lagirmaks@gmail.com>
2025-11-17 20:54:31 +0200

    Update Maksym.py
    Оптимізація вводу змінних

--- Maksym.py
+++ Maksym.py
@@ -1,10 +1,7 @@
 class Solution:
     def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
-        flowerbed.insert(0, 0)
-        flowerbed.append(0)
-        flowerbed.append(1)
-        counter = 0
-        zero_num = 0
+        flowerbed = [0] + flowerbed + [0,1]
+        counter, zero_num = 0, 0
         for i in flowerbed:
             if not i:
                 zero_num += 1
